149d316ebd9609cfbb7362e3370cf843a9986b2d
jaishankarreddy <159279823+jaishankarreddy@users.noreply.github.com>
2025-07-30 17:59:23 +0530

    config to connect to mongo DB

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8080;
+const url = process.env.MONGODB_URL;
 
 app.get("/", (req, res) => {
-  res.send("hallo");
+  res.send("hello");
 });
 
+function connectToMongoDB() {
+  mongoose.connect(url);
+  console.log("✅connected to db");
+}
+
 app.listen(port, () => {
+  connectToMongoDB();
   console.log(`App running on port ${port}`);
 });
